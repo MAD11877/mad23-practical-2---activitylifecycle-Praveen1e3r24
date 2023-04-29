b5269ce9ad2b7458fe2c345956d5ab48cee8898b
s10243638 <s10243638@connect.np.edu.sg>
2023-04-29 22:42:09 +0800

    week 1 and 2 practical

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -3,6 +3,9 @@ package sg.edu.np.mad.madpractical;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       User user = new User("praveen","Male",1,true);
+
+        Button follow = findViewById(R.id.button1);
+
+        if (user.followed)
+        {
+            follow.setText("Follow");
+        }
+        else
+        {
+            follow.setText("Unfollow");
+        }
+
+        follow.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (user.followed)
+                        {
+                         user.followed=false;
+                         follow.setText("Unfollow");
+                        }
+                        else {
+                            user.followed=true;
+                            follow.setText("Follow");
+                        }
+                    }
+                }
+
+        );
+
+
+
     }
+
+
 }
